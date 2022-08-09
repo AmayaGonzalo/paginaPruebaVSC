1d7c322da086fa3804c1e3719bfafbd11cad1b16
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-08-08 21:44:53 -0300

    quintoComit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-let contar = 0;
+let contador = 0;
+let imagen = document.querySelector("img");
+let parr = document.getElementById("texto");
 
-function contarClicks (){
-    contar++;
-    console.log("se hicieron", contar, "clicks");
+function contarClicks() {
+  contador++;
+  document.getElementById("texto").innerHTML =
+    "Se realizaron " + contador + " clicks";
+if(contador % 2 == 0){
+    imagen.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsxkoakzSU1QmK3TunPMuju0FI9jIGEmZ6sQ&usqp=CAU";
+    texto.style.color="blue";
+}else{
+    imagen.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8z3NbDfameYtWnkAvaKuKBh78evwI5N5RzA&usqp=CAU";
+    texto.style.color="red";
+  
 }
 
-
+  //console.log("hiciste", contador, "click");
+}
